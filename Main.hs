1ad502bd4e9ef20e4d10fe5562498e53ad347082
Shashank Pathak <pathakshashank97@gmail.com>
2024-02-16 11:23:41 +0000

    main shows all parse trees

--- Main.hs
+++ Main.hs
@@ -17,21 +17,24 @@ main = do
   let lean = head $ languages leanGr
   x <- readFile "prompt.txt"
   let s = preprocess x
-  putStr ("Preprocessing: " ++ s) 
-  let tree = head $ parse engGr eng (extract $ readType "text") s
+  putStr ("Preprocessing: " ++ s)
+  let trees =  parse engGr eng (extract $ readType "text") s
 
   -- Giving names to all notions 
-  let namedTree = changeMetaToInt tree
-  putStrLn ("Added variable names: " ++ linearize engGr eng namedTree)
+  let namedTrees = map changeMetaToInt trees
+  let linNamedTrees = map (\x -> "Added variable names: " ++ linearize engGr eng x) namedTrees
+  mapM_ putStrLn linNamedTrees
 
   -- simplification
-  let simplifiedTree = Texts.gf $ simplifyText (Texts.fg namedTree :: Texts.Gtext)
-  putStrLn ("Simplified: " ++ linearize engGr eng simplifiedTree)
+  let simplifiedTrees = map (\x -> Texts.gf $ simplifyText (Texts.fg x :: Texts.Gtext)) namedTrees
+  let linSimplifiedTrees = map (\x -> "Simplified: " ++ linearize engGr eng x) simplifiedTrees
+  mapM_ putStrLn linSimplifiedTrees
+
 
   -- translating
-  let translatedTree = Blocks.gf $ translateText (Texts.fg simplifiedTree :: Texts.Gtext)
-  let rawOutput = linearize leanGr lean translatedTree
+  let translatedTrees = map (\x -> Blocks.gf $ translateText (Texts.fg x :: Texts.Gtext)) simplifiedTrees
+  let rawOutputs = map (linearize leanGr lean) translatedTrees
 
   -- outputting after post-processing
-  putStrLn ("Translated: "++ postprocess rawOutput)
-  return ()
+  let linPostprocessed = map (\x -> "Translated: " ++ postprocess x) rawOutputs
+  mapM_ putStrLn linPostprocessed
